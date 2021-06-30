6dcebe52a55cf82c1c928bcea596c9c230859030
Abhishek Shinde <49138785+shindeabhi27@users.noreply.github.com>
2021-06-30 07:30:42 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const handlebars = require('express-handlebars');
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', handlebars({
+    layoutsDir: `${__dirname}/views/layouts`,
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: `${__dirname}/views/partials`,
+}));
+
+app.use(express.static('public'));
+
+app.get('/contactus', (req, res) => {
+    res.render('Contact us', {layout: 'Contact us'}); 
+});
+
+app.get('/work', (req, res) => {
+    res.render('Work', {layout: 'Portfolio'}); 
+});
+
+app.get('/about', (req, res) => {
+    res.render('About us', {layout: 'About Me'}); 
+});
+
+app.get('/', (req, res) => {
+    res.render('main', {layout: 'index'}); 
+});
+
+app.listen(port, () => {
+    console.log(`App listening to port ${port}`);
+});
